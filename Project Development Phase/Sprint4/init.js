3abc9aecda66ea106f631f49307fdda186bae56d
Daadreyaa D <daadreyaa@gmail.com>
2022-11-28 17:03:22 +0530

    Sprint 4

---
+++ Project Development Phase/Sprint4/init.js
@@ -0,0 +1,15 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCCrExzPjoIkeFMscZcL3jh5mbhkCR7lS0",
+    authDomain: "nalaiyathiran-71320.firebaseapp.com",
+    databaseURL: "https://nalaiyathiran-71320-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "nalaiyathiran-71320",
+    storageBucket: "nalaiyathiran-71320.appspot.com",
+    messagingSenderId: "811768493085",
+    appId: "1:811768493085:web:85bee3176c6b027fb7c2f4",
+    measurementId: "G-XKE62N9EJ7"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+var ref = firebase.database().ref();
